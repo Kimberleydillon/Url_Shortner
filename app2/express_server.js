e3a2a6591559681e75678d0b968f01d01bf54b42
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-11-05 00:09:22 +0000

    Completed Tiny App, user registration + cookies added

--- app2/express_server.js
+++ app2/express_server.js
@@ -1,10 +1,10 @@
+"use strict"
 //main variables
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
 
-//functions
 function generateRandomString() {
   let text = "";
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -12,7 +12,6 @@ function generateRandomString() {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   return text;
 }
-
 
 app.set("view engine", "ejs");
 
@@ -28,27 +27,25 @@ let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-//routes
 app.get("/", (req, res) => {
-  let templateVars = {
-    username: req.cookies["username"]
-  }
-  res.end("Get Shorty Homepage", templateVars);
+  res.end("Shorty Homepage");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    username: req.cookies["username"],
     };
+  //   if (typeof req.cookies["username"] !== 'undefined' ) {
+  //   templateVars.loggedin = true;
+  // } else {
+  //   templateVars.loggedin = false;
+  // }
   res.render("urls_index", templateVars);
-
 });
 
 app.get("/urls/new", (req, res) => {
@@ -71,6 +68,11 @@ app.post("/urls", (req, res) => {
  res.redirect("/urls");
 })
 
+app.get("/:id", (req, res) => {
+  let shortURL = req.params.id;
+  res.redirect(urlDatabase[shortURL]);
+});
+
 app.post('/:id/delete', (req,res) => {
 delete urlDatabase[req.params.id];2
 res.redirect("/urls");
@@ -82,14 +84,21 @@ res.redirect("/urls");
 })
 
 app.post('/login', (req,res) =>{
+  console.log("entered log in route");
   res.cookie("username", req.body.username);
-  res.redirect('/');
+  res.redirect('/urls');
 })
+
+app.post('/logout', (req,res)=>{
+res.clearCookie("username");
+res.redirect('/urls');
+});
 
 // app.get('/cookietester',(req,res)=>{
 //   console.log("username", req.cookies);
 //   res.redirect('/');
 // })
+
 app.listen(PORT, () => {
  console.log(`Example app listening on port ${PORT}!`);
 });
